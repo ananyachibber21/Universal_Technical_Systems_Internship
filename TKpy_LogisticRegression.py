7d10794c68045d2d9fe676aa89ff048a3b6d17b7
Ananya <ananyaunique21@gmail.com>
2022-06-23 12:06:02 +0530

    Create TKpy_LogisticRegression.py

---
+++ TKpy_LogisticRegression.py
@@ -0,0 +1,53 @@
+# Importing the libraries
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn import metrics
+
+# Load the dataset
+dataset = pd.read_csv(r"C:\Users\DELL\Documents\Jupyter\Machine Learning Projects\Brand Sale Prediction\sale_data.csv")
+
+# Segregating the dataset into independent variable x and dependent variable y
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
+
+# Split the data for training and testing
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.25, random_state=42)
+
+# Feature Scaling - we scale our data to make all the featured contribute equally to the result
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.fit_transform(x_test)
+
+# Train the algorithm
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+model.fit(x_train, y_train)
+
+# Prediction for all Test Data
+y_pred = model.predict(x_test)
+
+class FunctionInfo:
+	
+	def __init__(self):
+		
+		# Function information dictionary - { function name: [ list of arguments, return type, funtion description ] }
+		
+		self.func_dict =   {	
+            
+                                'TKPY_logistic_pred': [['array'], ['float'], ['Predicted values']],
+                                'TKPY_logistic_Confusion': [['int'], ['matrix'], ['Confusion Matrix']]
+							}
+	def get_func_dict(self):
+		return self.func_dict
+
+def TKPY_logistic_pred(dummy):
+	a = model.predict(sc.transform([dummy]))
+	return float(a)
+
+def TKPY_logistic_Confusion(dummy):
+	a = confusion_matrix(y_test,y_pred)
+	a = a + dummy - dummy
+	return np.array(a)
